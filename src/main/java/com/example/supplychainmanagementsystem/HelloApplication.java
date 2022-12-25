bc96a02c2d6e845d2414a0e9d363b8642f4698dd
AbhishekF <abhishek.fopse@gmail.com>
2022-12-25 12:41:44 +0530

    LoginPageAdded

--- src/main/java/com/example/supplychainmanagementsystem/HelloApplication.java
+++ src/main/java/com/example/supplychainmanagementsystem/HelloApplication.java
@@ -3,18 +3,42 @@ package com.example.supplychainmanagementsystem;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
 
+    private GridPane loginPage(){
+        Label emailLabel = new Label("Email");
+        Label passwordLabel = new Label("Password:");
+
+        TextField emailTextField = new TextField();
+        PasswordField passwordTextField = new PasswordField();
+
+        GridPane gridPane = new GridPane();
+        gridPane.add(emailLabel,0,0);
+        gridPane.add(emailTextField,1,0);
+        gridPane.add(passwordLabel,0,1);
+        gridPane.add(passwordTextField,1,1);
+
+        return gridPane;
+
+    }
+
     private Pane createContent(){
         Pane root = new Pane();
 
+        root.getChildren().addAll(loginPage());
         return root;
     }
+
     @Override
     public void start(Stage stage) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
