bc4bc0a5a2d03b85605a552ce7b19a982db7fb07
AbhishekF <abhishek.fopse@gmail.com>
2023-01-09 17:05:04 +0530

    Footer Added

--- src/main/java/com/example/supplychainmanagementsystem/SupplyChain.java
+++ src/main/java/com/example/supplychainmanagementsystem/SupplyChain.java
@@ -19,13 +19,40 @@ public class SupplyChain extends Application {
 
     public static final int width = 700, height = 600, headerBar = 50;
     Pane bodyPane = new Pane();
+
     Login login = new Login();
     ProductDetails productDetails = new ProductDetails();
+
+    Button globalLoginButton;
+    Label customerEmailLabel = null;
+    String customerEmail = null;
+
 
 
     private GridPane headerBar(){
         TextField searchText = new TextField();
         Button searchButton = new Button("Search");
+        searchButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                String productName = searchText.getText();
+                //clear body and put this new pane in the body
+                bodyPane.getChildren().clear();
+                bodyPane.getChildren().add(productDetails.getProductsByName(productName));
+            }
+        });
+
+        globalLoginButton = new Button("Log In");
+        globalLoginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                bodyPane.getChildren().clear();
+                bodyPane.getChildren().add(loginPage());
+                globalLoginButton.setDisable(true);
+
+            }
+        });
+        customerEmailLabel = new Label("Welcome User");
 
         GridPane gridPane = new GridPane();
         gridPane.setMinSize(bodyPane.getMinWidth(),headerBar-10);
@@ -35,6 +62,8 @@ public class SupplyChain extends Application {
         gridPane.setHgap(5);
         gridPane.add(searchText,0,0);
         gridPane.add(searchButton,1,0);
+        gridPane.add(globalLoginButton,2,0);
+        gridPane.add(customerEmailLabel,3,0);
         return gridPane;
     }
     private GridPane loginPage(){
@@ -52,6 +81,11 @@ public class SupplyChain extends Application {
                 String password = passwordTextField.getText();
                 if(login.customerLogin(email,password)){
                     messageLabel.setText("Login Successful");
+                    customerEmail = email;
+                    globalLoginButton.setDisable(true);
+                    customerEmailLabel.setText("Welcome : "+ customerEmail);
+                    bodyPane.getChildren().clear();
+                    bodyPane.getChildren().add(productDetails.getAllProducts());
                 } else {
                     messageLabel.setText("Login Failed");
                 }
@@ -74,17 +108,47 @@ public class SupplyChain extends Application {
         return gridPane;
 
     }
+    private GridPane footerBar(){
+        Button addToCartButton = new Button("Add To Cart");
+        Button buyNowButton = new Button("Buy Now");
+        Label messageLabel = new Label("");
+        buyNowButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Product selectedProduct = productDetails.getSelectedProduct();
+                if(Order.placeOrder(customerEmail,selectedProduct)){
+                    messageLabel.setText("Ordered");
+                } else {
+                    messageLabel.setText("Order Failed");
+                }
+            }
+        });
 
+
+
+        GridPane gridPane = new GridPane();
+        gridPane.setMinSize(bodyPane.getMinWidth(),headerBar-10);
+//        gridPane.setStyle("-fx-background-color: #C0C0C0");
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setTranslateY(headerBar+height+5);
+        gridPane.setVgap(5);
+        gridPane.setHgap(20);
+        gridPane.add(addToCartButton,0,0);
+        gridPane.add(buyNowButton,1,0);
+        gridPane.add(messageLabel,2,0);
+
+        return gridPane;
+    }
     private Pane createContent(){
         Pane root = new Pane();
-        root.setPrefSize(width,height+headerBar);
+        root.setPrefSize(width,height+2*headerBar);
 
         bodyPane.setMinSize(width,height);
         bodyPane.setTranslateY(headerBar);
         bodyPane.getChildren().addAll(productDetails.getAllProducts());
 
 
-        root.getChildren().addAll(headerBar(),bodyPane);
+        root.getChildren().addAll(headerBar(),bodyPane, footerBar());
         return root;
     }
 
